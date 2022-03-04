374c98e804f479e90e209d24f1b90aadd95714b0
SerikovAleksey <serikov.ar@phystech.edu>
2022-03-04 22:50:46 +0300

    update

--- laba1_2_2/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    double m, r, g = 9.81, b, M = 0.2 * 0.01;
-    std::cin >> m >> r >> b;
-    std::cout << (m*g*r*0.00001 - M)/b<< std::endl;
-    return 0;
-}
