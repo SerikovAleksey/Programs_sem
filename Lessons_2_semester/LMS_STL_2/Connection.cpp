3bca1578ff0f27250ddfd16a7b9243ed62935edd
SerikovAleksey <serikov.ar@phystech.edu>
2022-04-05 16:40:42 +0300

    ex5_lms_done

--- Lessons_2_semester/LMS_STL_2/Connection.cpp
+++ Lessons_2_semester/LMS_STL_2/Connection.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <map>
 #include <set>
-
+#include <algorithm>
 using std::string;
 
 class Connection
@@ -49,28 +49,50 @@ public:
 
     // Вызов этого метода уведомляет анализатор о новом подключении.
     void handleConnection(const Connection& c) {
-        times.second.push_back(c.getTimestamp());
-        ports.second.insert(c.getPort());
+        times[c.getSource()].insert(c.getTimestamp());
+        ports[c.getSource()].insert(c.getPort());
     }
 
     // Проверить, является ли указанный адрес нарушителем
     bool isIntruder(const string& source) {
-        int i = 0, j = 1;
-        for (auto it = std::begin(times.second); it != std::end(times.second); it++) {
-            if (*it != *(std::prev(std::end(times.second), 1)) && *it - *(std::next(it, 1)) < time) {
-                i += *it - *(std::next(it, 1));
-                j++;
-                std::cout << "it = " << *it << " " << *(std::next(it, 1))<< std::endl;
+        if (port > ports[source].size()) {
+            return false;
+        }
+        if (times[source].size() < ports[source].size()) {
+            return true;
+        }
+        int i = 0, j = 1, dur = 0;
+        for (auto it = std::begin(times[source]); it != std::end(times[source]); it++) {
+//            std::cout << "times = " << *it << " ";
+            if (dur < time) {
+                if (*it - i < time) {
+                    dur += *it - i;
+                    j++;
+//                    std::cout << "dur = " << dur << std::endl;
+                }
+            }
+            i = *it;
+
+            if (dur < time && j >= port) {
+                times[source].clear();
+                ports[source].clear();
+                return true;
             }
         }
-        std::cout << "i= " << i << " " << "j= " << j << std::endl;
-        if (i > time && j <= port)return true;
+        times[source].clear();
+        ports[source].clear();
         return false;
     }
 
+//    void erase_prev_user (const string& sourse) {
+//        for (auto item: times[sourse]){
+//            times[sourse].erase(item);
+//        }
+//    }
+
 private:
-    std::pair<std::string, std::set<unsigned long long>> ports;
-    std::pair<std::string, std::vector<unsigned long long>> times;
+    std::map<std::string, std::set<int>> ports;
+    std::map<std::string, std::set<int>> times;
     unsigned  short time;
     unsigned  short port;
 };
@@ -80,6 +102,35 @@ int main () {
     IntrusionDetector id;
     id.setTimeThreshold(5);
     id.setPortLimit(3);
+    id.handleConnection({"evil.com", 21, 100500});
+    id.handleConnection({"evil.com", 22, 100512});
+    id.handleConnection({"evil.com", 23, 100511});
+    id.handleConnection({"evil.com", 24, 100510});
+    id.handleConnection({"evil.com", 25, 100600});
+    cout << boolalpha << "Checking if evil.com is intruder: " << id.isIntruder("evil.com") << endl;
+
+id.handleConnection({"load.com", 21, 100500});
+id.handleConnection({"load.com", 22, 100600});
+id.handleConnection({"load.com", 23, 100700});
+id.handleConnection({"load.com", 24, 100800});
+id.handleConnection({"load.com", 25, 100900});
+cout << boolalpha << "Checking if load.com is intruder: " << id.isIntruder("load.com") << endl;
+//
+id.handleConnection({"load.com", 80, 100500});
+id.handleConnection({"load.com", 80, 100501});
+id.handleConnection({"load.com", 80, 100502});
+id.handleConnection({"load.com", 80, 100503});
+id.handleConnection({"load.com", 80, 100504});
+cout << boolalpha << "Checking if load.com is intruder: " << id.isIntruder("load.com") << endl;
+
+cout << boolalpha << "Checking if evil.com is intruder: " << id.isIntruder("test.com") << endl;
+
+    id.handleConnection({"evil.com", 21, 100500});
+    id.handleConnection({"evil.com", 22, 100501});
+    id.handleConnection({"evil.com", 23, 100502});
+    id.handleConnection({"evil.com", 24, 100503});
+    id.handleConnection({"evil.com", 25, 100504});
+    cout << boolalpha << "Checking if evil.com is intruder: " << id.isIntruder("evil.com") << endl;
 
     id.handleConnection({"evil.com", 21, 100500});
     id.handleConnection({"evil.com", 22, 100600});
@@ -88,10 +139,4 @@ int main () {
     id.handleConnection({"evil.com", 25, 100700});
     cout << boolalpha << "Checking if evil.com is intruder: " << id.isIntruder("evil.com") << endl;
 
-//    id.handleConnection({"load.com", 21, 100500});
-//    id.handleConnection({"load.com", 22, 100600});
-//    id.handleConnection({"load.com", 23, 100700});
-//    id.handleConnection({"load.com", 24, 100800});
-//    id.handleConnection({"load.com", 25, 100900});
-//    cout << boolalpha << "Checking if load.com is intruder: " << id.isIntruder("load.com") << endl;
 }
